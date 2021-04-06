2e902de35a918e21a2766f3078a72b33d5f575f3
Vidit Singh <viditsingh329@outlook.com>
2021-04-06 10:26:23 +0530

    final commit

--- projfrontend/src/components/Home.js
+++ projfrontend/src/components/Home.js
@@ -25,6 +25,9 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import pdf from '../assets/MyResume.pdf';
+import BITBOX from '../assets/BITBOX.png';
+import BU_Hack from '../assets/BU_Hack.jpeg';
 const Home=()=>{
     const useStyles = makeStyles((theme) => ({
         paper: {
@@ -37,15 +40,16 @@ const Home=()=>{
       }));
       const classes = useStyles();
 
-      const achievement=[{"year":"2019-2020","heading":"The world","cgpa":"6.13","institute":"CMS"},
-                          {"year":"2018-2019","heading":"The world hello","cgpa":"9","institute":"SVNIT"},
-                          {"year":"2019-2020","heading":"The world","cgpa":"6.13","institute":"CMS"},
-                          {"year":"2018-2019","heading":"The world hello","cgpa":"9","institute":"SVNIT"}]
+      const achievement=[{"year":"26th-28th March, 2021","heading":"Top 10 in BITBOX","cgpa":"","institute":"(Hackathon organised by JIIT)"},
+                          {"year":"March 2018","heading":"5th Position in Hertz","cgpa":"","institute":"Electronics Engineering Department, NIT Surat "},
+                          {"year":" April 2019","heading":"8th Position in Inception 3.1","cgpa":"","institute":"ACM, NIT Surat"}]
 
-      const project=[{"heading":"VSG1","description":"kergsbjbejskrbgdvjsbgevd","src":Profile,"glink":"", "link":""},
-      {"heading":"VSG2","description":"jkngjhgjked","src":Logo,"glink":"", "link":""}
+      const project=[{"heading":"Food Finders","description":"It is a platform which will provide reliable and nutrious food to undernourished and malnourished people in order to eliminate the problem of HUNGER from our society.","src":BITBOX,"glink":"https://github.com/viditrv123/BitBox", "link":""},
+      {"heading":"Healing Equilibrium","description":"It is an effort to ensure everyone's safety.This is a better alternative providing health checkup at your place with live interaction with highly experienced doctors from all over the country.","src":BU_Hack,"glink":"https://github.com/viditrv123/BU-Hack", "link":""}]
+
+      const work=[{"year":"June-Aug 2020", "heading":"Web Development Intern","place":"Rexzet Solution"},
+      {"year":"Oct’19-May ’20", "heading":"Head, Web Development Team","place":"Research and Innovation Affairs Council, NIT Surat"}
     ]
-
 return(<div>
 
     
@@ -66,7 +70,7 @@ return(<div>
     <a href="https://codeforces.com/profile/vidit_rv123"><SiCodeforces size="2em" /></a>
     <a href="https://www.topcoder.com/members/vidit_rv123"><SiTopcoder size="2em" /></a>
     <a href="mailto:viditsingh329@gmail.com?subject=Mail from Our Site"><AiFillMail size="2em" /></a>
-    <a href="www.facebook.com"><AiFillFilePdf size="2em" /></a>
+    <a href={pdf} download><AiFillFilePdf size="2em" /></a>
     </div>
     <Image src={Logo} style={{"width":"100px","height":"100px","display": "block","marginRight":"auto","marginLeft":"auto"}}></Image>
     </div>
@@ -99,8 +103,8 @@ return(<div>
         </CardContent>
       </CardActionArea>
       <CardActions>
-      <a href="www.facebook.com"><AiFillGithub size="2em" /></a>
-      <a href="www.facebook.com"><BiLink size="2em" /></a>
+      <a href={value.glink}><AiFillGithub size="2em" /></a>
+      <a href={value.link}><BiLink size="2em" /></a>
       
       </CardActions>
     </Card>
@@ -109,9 +113,100 @@ return(<div>
   </div>)
     })}
       </div>
+      <hr></hr>
     </section>
     <section id="achievement" style={{"background-color": "#f5f186",
-"background-image": "linear-gradient(315deg, #f5f186 0%, #9dfbc8 74%)"}}> <h3 style={{"textAlign":"center", "textDecoration":"underline"}}>Achievements</h3>
+"background-image": "linear-gradient(315deg, #f5f186 0%, #9dfbc8 74%)", "padding":"0px"}}> <h3 style={{"textAlign":"center", "textDecoration":"underline"}}>Work Experience</h3>
+<Timeline align="alternate">
+{work.map((value, key)=>{
+  if(key%2==0){
+  return(<div><TimelineItem>
+        <TimelineOppositeContent>
+          <Typography variant="body2" color="textSecondary">
+            {value.year}
+          </Typography>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot>
+            <GrAchievement />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+            {value.heading}
+            </Typography>
+            <Typography><br></br>{value.place}</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem></div>)
+  }
+  else{
+    return(<div><TimelineItem>
+        
+        
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+            {value.heading}
+            </Typography>
+            <Typography><br></br>{value.place}</Typography>
+          </Paper>
+        </TimelineContent>
+        <TimelineSeparator>
+          <TimelineDot>
+            <GrAchievement />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+      <TimelineOppositeContent>
+          <Typography variant="body2" color="textSecondary" style={{"textAlign":"left"}}>
+            {value.year}
+          </Typography>
+        </TimelineOppositeContent>
+      </TimelineItem>
+      </div>)
+  }
+})}
+      
+      </Timeline>
+      <hr></hr>
+      </section>
+      <section id="skills" style={{"background-color": "#abe9cd",
+"background-image": "linear-gradient(315deg, #abe9cd 0%, #3eadcf 74%)"}}>
+<div style={{"marginLeft":"10px"}}>
+        <h3 style={{"textAlign":"center", "textDecoration":"underline"}}>Skills</h3>
+        <br></br>
+        <h6>Programming Languages</h6>
+        
+        <p>C , C++ , Java , Python , Javascript</p>
+        <br></br>
+        <h6>Frontend Development</h6>
+        
+        <p> HTML , CSS , Bootstrap , React.js , Angular.js</p>
+        <br></br>
+        <h6>Backend Development</h6>
+        
+        <p> Node.js</p>
+        <br></br>
+        <h6>Database</h6>
+        
+        <p> SQL , MongoDB</p>
+        <br></br>
+        <h6>Machine Learning</h6>
+        
+        <p> Neural Network & Fuzzy Logic , Tensorflow.js</p>
+        <br></br>
+        <h6>Others</h6>
+        
+        <p>  MATLAB , Competitive Programming , Optimization , Image Processing , Computer Vision , AutoCAD , Ethereum Blockchain</p>
+        <br></br>
+        </div>
+        <hr></hr>
+      </section>
+    <section id="achievement" style={{"background-color": "#f9484a",
+"background-image": "linear-gradient(315deg, #f9484a 0%, #fbd72b 74%)"}}> <h3 style={{"textAlign":"center", "textDecoration":"underline"}}>Achievements</h3>
 <Timeline align="alternate">
 {achievement.map((value, key)=>{
   if(key%2==0){
