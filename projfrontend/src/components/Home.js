ca58140aa2ebd2229a2f1526026aff372e66d037
Vidit Singh <viditsingh329@outlook.com>
2021-04-06 07:39:43 +0530

    Major Change commit

--- projfrontend/src/components/Home.js
+++ projfrontend/src/components/Home.js
@@ -12,13 +12,19 @@ import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
-import LaptopMacIcon from '@material-ui/icons/LaptopMac';
-import HotelIcon from '@material-ui/icons/Hotel';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import { AiFillGithub, AiFillLinkedin, AiOutlineTwitter, AiFillFilePdf } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin, AiOutlineTwitter, AiFillFilePdf, AiFillMail } from 'react-icons/ai';
 import {FaBloggerB} from 'react-icons/fa'
 import {SiHackerearth, SiHackerrank, SiCodechef, SiCodeforces, SiTopcoder} from 'react-icons/si'
+import {BiLink} from 'react-icons/bi'
+import {GrAchievement} from 'react-icons/gr'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 const Home=()=>{
     const useStyles = makeStyles((theme) => ({
         paper: {
@@ -31,7 +37,14 @@ const Home=()=>{
       }));
       const classes = useStyles();
 
-      const achievement=[{}]
+      const achievement=[{"year":"2019-2020","heading":"The world","cgpa":"6.13","institute":"CMS"},
+                          {"year":"2018-2019","heading":"The world hello","cgpa":"9","institute":"SVNIT"},
+                          {"year":"2019-2020","heading":"The world","cgpa":"6.13","institute":"CMS"},
+                          {"year":"2018-2019","heading":"The world hello","cgpa":"9","institute":"SVNIT"}]
+
+      const project=[{"heading":"VSG1","description":"kergsbjbejskrbgdvjsbgevd","src":Profile,"glink":"", "link":""},
+      {"heading":"VSG2","description":"jkngjhgjked","src":Logo,"glink":"", "link":""}
+    ]
 
 return(<div>
 
@@ -39,97 +52,121 @@ return(<div>
     
 <div className="row">
 <div className="col-md-4" >
-<br></br><br></br>
+<br></br>
     <Image src={Profile} className="avatar" style={{"width":"200px", "height":"200px","display": "block","marginRight":"auto","marginLeft":"auto"}} roundedCircle />
-    <h1 style={{"textAlign":"center"}}>Hello World!<br /></h1><h2 style={{"textAlign":"center"}}>I am <br /> Vidit Singh Gautam</h2>
-    <div style={{"display": "block","marginLeft":"40px", "marginRight":"35px"}}>
-    <a href="www.facebook.com"><AiFillGithub size="2em" /></a>
-    <a href="www.facebook.com"><AiFillLinkedin size="2em" /></a>
-    <a href="www.facebook.com"><AiOutlineTwitter size="2em" /></a>
-    <a href="www.facebook.com"><FaBloggerB size="2em" /></a>
-    <a href="www.facebook.com"><SiHackerearth size="2em" /></a>
-    <a href="www.facebook.com"><SiHackerrank size="2em" /></a>
-    <a href="www.facebook.com"><SiCodechef size="2em" /></a>
-    <a href="www.facebook.com"><SiCodeforces size="2em" /></a>
-    <a href="www.facebook.com"><SiTopcoder size="2em" /></a>
+    <h1 style={{"textAlign":"center"}}>Hello World!<br /></h1><h2 style={{"textAlign":"center"}}>I am <br /> Vidit Singh Gautam</h2><br /><p style={{"textAlign": "center"}}>A Full Stack developer, Blockchain developer, Competitive Coder, based in Lucknow, India.</p>
+    <div style={{"display": "block","marginLeft":"30px"}}>
+    <a href="https://github.com/viditrv123"><AiFillGithub size="2em" /></a>
+    <a href="https://www.linkedin.com/in/vidit-singh-gautam-642290178/"><AiFillLinkedin size="2em" /></a>
+    <a href="https://twitter.com/ViditSinghGaut1"><AiOutlineTwitter size="2em" /></a>
+    <a href="https://www.blogger.com/blog/posts/387435847157758210"><FaBloggerB size="2em" /></a>
+    <a href="https://www.hackerearth.com/@viditsingh329"><SiHackerearth size="2em" /></a>
+    <a href="https://www.hackerrank.com/vidit_rv123"><SiHackerrank size="2em" /></a>
+    <a href="https://www.codechef.com/users/vidit_rv123"><SiCodechef size="2em" /></a>
+    <a href="https://codeforces.com/profile/vidit_rv123"><SiCodeforces size="2em" /></a>
+    <a href="https://www.topcoder.com/members/vidit_rv123"><SiTopcoder size="2em" /></a>
+    <a href="mailto:viditsingh329@gmail.com?subject=Mail from Our Site"><AiFillMail size="2em" /></a>
     <a href="www.facebook.com"><AiFillFilePdf size="2em" /></a>
     </div>
-    <br></br>
-    <p></p>
+    <Image src={Logo} style={{"width":"100px","height":"100px","display": "block","marginRight":"auto","marginLeft":"auto"}}></Image>
     </div>
     
     <div className="col-md-8" style={{"overflowY":"scroll", "overflowY":"auto","width":"100%","height":"600px","padding":"0px"}}>
-    <section id="education" style={{"background-color": "#f5f186",
-"background-image": "linear-gradient(315deg, #f5f186 0%, #9dfbc8 74%)"}}> <h3>Education</h3>
-<Timeline align="alternate">
+    <section id="projects" style={{"background-color": "#c7e9fb",
+"background-image": "linear-gradient(315deg, #c7e9fb 0%, #e61d8c 74%)"
+}}>
+<h3 style={{"textAlign":"center", "textDecoration":"underline"}}>Projects</h3>
+    <div className="row">
+    {project.map((value,key)=>{
+      return(<div className="col-md-4">
+    <br/><Card style={{"width":"100%","marginLeft":"10px"}} >
+      <CardActionArea>
+        <CardMedia
+          style={{"height":"250px", "width":"100%"}}
+          
+          image={value.src}
 
-      <TimelineItem>
+          
+        />
+        
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {value.heading}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {value.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <a href="www.facebook.com"><AiFillGithub size="2em" /></a>
+      <a href="www.facebook.com"><BiLink size="2em" /></a>
+      
+      </CardActions>
+    </Card>
+    <br />
+    
+  </div>)
+    })}
+      </div>
+    </section>
+    <section id="achievement" style={{"background-color": "#f5f186",
+"background-image": "linear-gradient(315deg, #f5f186 0%, #9dfbc8 74%)"}}> <h3 style={{"textAlign":"center", "textDecoration":"underline"}}>Achievements</h3>
+<Timeline align="alternate">
+{achievement.map((value, key)=>{
+  if(key%2==0){
+  return(<div><TimelineItem>
         <TimelineOppositeContent>
           <Typography variant="body2" color="textSecondary">
-            2017-2021
+            {value.year}
           </Typography>
         </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineDot>
-            <FastfoodIcon />
+            <GrAchievement />
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
           <Paper elevation={3} className={classes.paper}>
             <Typography variant="h6" component="h1">
-            Sardar Vallabhbhai National Institute of Technology, Surat
+            {value.heading}
             </Typography>
-            <Typography><br></br><br></br>Bachelor of Technology, Mechanical Engineering </Typography>
-            <Typography><br></br><br></br>CGPA-6.30</Typography>
+            <Typography><br></br><br></br>{value.institute}</Typography>
+            <Typography><br></br><br></br>{value.cgpa}</Typography>
           </Paper>
         </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent>
-          <Typography variant="body2" color="textSecondary">
-            2015-2016
-          </Typography>
-        </TimelineOppositeContent>
+      </TimelineItem></div>)
+  }
+  else{
+    return(<div><TimelineItem>
+        
+        
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+            {value.heading}
+            </Typography>
+            <Typography><br></br><br></br>{value.institute}</Typography>
+            <Typography><br></br><br></br>{value.cgpa}</Typography>
+          </Paper>
+        </TimelineContent>
         <TimelineSeparator>
-          <TimelineDot color="primary">
-            <LaptopMacIcon />
+          <TimelineDot>
+            <GrAchievement />
           </TimelineDot>
           <TimelineConnector />
-          
         </TimelineSeparator>
-        <TimelineContent>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-            City Montessori School, Lucknow
-            </Typography>
-            <Typography><br></br><br></br>Indian School Certificate</Typography>
-            <Typography><br></br><br></br>Percentage: 91.5%</Typography>
-          </Paper>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
       <TimelineOppositeContent>
-          <Typography variant="body2" color="textSecondary">
-            2013-2014
+          <Typography variant="body2" color="textSecondary" style={{"textAlign":"left"}}>
+            {value.year}
           </Typography>
         </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
-          </TimelineDot>
-          <TimelineConnector className={classes.secondaryTail} />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="h1">
-            City Montessori School, Lucknow
-            </Typography>
-            <Typography><br></br><br></br>Indian Certificate of Secondary Education</Typography>
-            <Typography><br></br><br></br>Percentage: 91%</Typography>
-          </Paper>
-        </TimelineContent>
       </TimelineItem>
+      </div>)
+  }
+})}
+      
       </Timeline>
       <hr></hr>
       </section>
